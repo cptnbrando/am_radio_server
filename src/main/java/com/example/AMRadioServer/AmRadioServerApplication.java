763450b326451ac360b1e588f8559672e6fe99a8
Brando <82909107+cptnbrando@users.noreply.github.com>
2024-06-02 17:28:05 -0500

    .env man

--- src/main/java/com/example/AMRadioServer/AmRadioServerApplication.java
+++ src/main/java/com/example/AMRadioServer/AmRadioServerApplication.java
@@ -7,7 +7,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AmRadioServerApplication {
 
 	public static void main(String[] args) {
+		if(!checkForEnv()) return;
 		SpringApplication.run(AmRadioServerApplication.class, args);
+	}
+
+	public static boolean checkForEnv() {
+		if(System.getenv("RADIO_APP_URL") == null) {
+			printKingMessage();
+			System.out.println("Check dev.env-example and prod.env-example in the root of this project");
+			return false;
+		}
+
+		return true;
+	}
+
+	public static void printKingMessage() {
+		System.out.println("");
+		System.out.println("-----------------MESSAGE-----------------");
 	}
 
 }
