074023fc2c5da03752cb3f59a9f1001399138e17
Brando <82909107+cptnbrando@users.noreply.github.com>
2024-06-02 17:46:17 -0500

    .env File Messages created
    launched this thing up after 5 years and it still works like a machine, same stupid rhythm bugs and all. I just needed the .env file, thank god I backup stuff. Plugged it in and it works like a dream, unreal. Added error messages to detect .env files for next time.

--- src/main/java/com/example/AMRadioServer/AmRadioServerApplication.java
+++ src/main/java/com/example/AMRadioServer/AmRadioServerApplication.java
@@ -14,7 +14,11 @@ public class AmRadioServerApplication {
 	public static boolean checkForEnv() {
 		if(System.getenv("RADIO_APP_URL") == null) {
 			printKingMessage();
-			System.out.println("Check dev.env-example and prod.env-example in the root of this project");
+			System.out.println("dev.env NOT DETECTED");
+			System.out.println("Check dev.env-example in the root of this project");
+			System.out.println("Fill it out and change the filename to dev.env, then pass into Spring or Docker via command line.");
+			System.out.println("If using IntelliJ, you should make run configurations.");
+			System.out.println("If deploying on a cloud server, check out prod.env-example. Same deal but needs AWS stuff.");
 			return false;
 		}
 
